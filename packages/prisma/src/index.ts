7bb05b5a3366706d6384b99e474feb805a207bf3
Sarthak Yadav <sarthakyadav7837@gmail.com>
2026-06-06 15:04:45 +0530

    Fixed prisma client

--- packages/prisma/src/index.ts
+++ packages/prisma/src/index.ts
@@ -2,9 +2,7 @@ import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../generated/prisma/client";
 import dotenv from "dotenv";
 import path from "path";
-import { fileURLToPath } from "url";
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
 dotenv.config({ path: path.join(__dirname, "../.env") });
 
 const adapter = new PrismaPg({
